d267c7e37c7e4fa49e710d530e6f63573f52299f
Kneelawk <jedidiah.pommert@gmail.com>
2019-10-07 18:25:02 -0700

    Export more packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module com.kneelawk.klinesjava {
     exports com.kneelawk.klinesjava;
     exports com.kneelawk.klinesjava.buffers;
+    exports com.kneelawk.klinesjava.buffers.databuffer;
+    exports com.kneelawk.klinesjava.buffers.objectbuffer;
+    exports com.kneelawk.klinesjava.buffers.objectbuffer.data;
+    exports com.kneelawk.klinesjava.graphics;
     exports com.kneelawk.klinesjava.shaders;
+    exports com.kneelawk.klinesjava.utils;
 
     requires com.google.common;
 
